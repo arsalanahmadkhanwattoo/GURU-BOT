5ddffd17f07c272c0ade148cf148e52e77a4180a
arsalanahmadkhanwattoo <157638155+arsalanahmadkhanwattoo@users.noreply.github.com>
2024-01-27 11:01:53 +0500

    Update config.js

--- config.js
+++ config.js
@@ -9,10 +9,10 @@ import axios from 'axios'
 global.owner = [
   ['917605902011', 'GURU', true],
   ['6281255369012', 'Ash', true], 
-  [''] 
+  ['923055409641', 'arsal', true] 
 ] //Number of owners
 
-//global.pairingNumber = "" //put your bot number here
+//global.pairingNumber = "923055409641" //put your bot number here
 
 global.mods = ['917605902011','32460248586'] 
 global.prems = ['917605902011', '32460248586', '919398758484']
